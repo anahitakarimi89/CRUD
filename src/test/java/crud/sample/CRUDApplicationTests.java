ef40db04ffcdb123dbc793a9d6661a46beb34541
a_karimi <anahita.karimi6768@gmail.com>
2023-04-09 01:21:12 +0430

    add test

---
+++ src/test/java/crud/sample/CRUDApplicationTests.java
@@ -0,0 +1,12 @@
+package crud.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CRUDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
