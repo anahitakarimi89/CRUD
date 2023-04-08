d870a93777442023aece1a49864718b37046aa51
a_karimi <anahita.karimi6768@gmail.com>
2023-04-08 16:35:17 +0430

    add log

--- src/main/java/crud/sample/controller/BookController.java
+++ src/main/java/crud/sample/controller/BookController.java
@@ -2,6 +2,8 @@ package crud.sample.controller;
 
 import crud.sample.model.Book;
 import crud.sample.service.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -17,6 +20,8 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    private final Logger log = LoggerFactory.getLogger(BookController.class);
 
 
     @PostMapping("/saveBook")
@@ -29,7 +34,9 @@ public class BookController {
 
         }catch (Exception e){
 
-            //LOGBACK "problem in saving book"
+            log.info("problem in saving book");
+            log.error("problem in cause by"+e.getMessage());
+
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
@@ -42,7 +49,9 @@ public class BookController {
         try {
             returnResult = bookService.fetchBookList();
         }catch (Exception e){
-            //LOGBACK "problem in fetching book list"
+
+            log.info("problem in fetching book list");
+            log.error("problem in cause by"+e.getMessage());
         }
         return returnResult;
     }
@@ -56,7 +65,9 @@ public class BookController {
         try {
             returnValue = bookService.updateBook(bookId,book);
         }catch (Exception e){
-            //LOGBACK "problem in updating book"
+
+            log.info("problem in updating book");
+            log.error("problem in cause by"+e.getMessage());
         }
         return new ResponseEntity<>(returnValue, HttpStatus.OK);
 
@@ -70,7 +81,9 @@ public class BookController {
         try {
             bookService.deleteBook(bookId);
         }catch (Exception e){
-            //LOGBACK "problem in deleting book"
+
+            log.info("problem in deleting book");
+            log.error("problem in cause by"+e.getMessage());
         }
 
         return new ResponseEntity<>("Book deleted successfully!.", HttpStatus.OK);
