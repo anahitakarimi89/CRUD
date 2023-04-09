194be2efc6984c9b1b62c41951455a2e6aa5a9b1
a_karimi <anahita.karimi6768@gmail.com>
2023-04-09 19:22:48 +0430

    add security-in memory users

--- src/main/java/crud/sample/config/SecurityConfig.java
+++ src/main/java/crud/sample/config/SecurityConfig.java
@@ -3,10 +3,15 @@ package crud.sample.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -15,22 +20,44 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
-   @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable().authorizeHttpRequests()
+                .requestMatchers("/books/bookLists").permitAll()
+                .requestMatchers("/books/saveBook").hasRole("USER")
+                .requestMatchers("/books/updateBook").hasRole("ADMIN")
+                .requestMatchers("/books/deleteBook").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-                .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/books/**").permitAll()
-                                .anyRequest().authenticated()
+    public UserDetailsService userDetailsService() {
 
-                );
 
-        return http.build();
+        UserDetails theUser = User.withUsername("user")
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+                .password("12345678").roles("USER").build();
+
+
+        UserDetails theManager = User.withUsername("admin")
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+                .password("87654321").roles("ADMIN").build();
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        userDetailsManager.createUser(theUser);
+        userDetailsManager.createUser(theManager);
+
+        return userDetailsManager;
     }
+
+
 }
